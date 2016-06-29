b1fdf28d8b9ada20f64a373c5bae45164eb79f91
Marcin Badurowicz <m@badurowicz.net>
2016-06-29 21:39:55 +0200

    changes in assemblyinfo, updating to version 1.1

--- Fa287aDriver/Properties/AssemblyInfo.cs
+++ Fa287aDriver/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fa287aDriver")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FA287A for Windows Driver")]
+[assembly: AssemblyDescription("Windows handler application for HP FA287A Bluetooth SPP keyboard")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Fa287aDriver")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("ktos.info")]
+[assembly: AssemblyProduct("FA287A for Windows")]
+[assembly: AssemblyCopyright("Copyright © Marcin Badurowicz 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
